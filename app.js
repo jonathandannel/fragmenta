3832493f3b7a3a1abf77bb3bc35659f525e9b9f9
jonathan dannel <usixjad@gmail.com>
2019-12-11 02:07:45 -0500

    test db/dotenv

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const mysql = require("mysql");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect();
+
+db.query("SELECT 1 + 1 AS solution", function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log("The solution is: ", rows[0].solution);
+});
+
+db.end();
 
 require("dotenv").config();
 app.use(logger("dev"));
