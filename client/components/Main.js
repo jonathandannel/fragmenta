6059d8a1216b1b3f2a159964fd635f27baf9a096
jonathan dannel <usixjad@gmail.com>
2019-12-21 23:12:59 -0500

    nest routes

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,7 +1,7 @@
 import { createElement as h } from "react";
 import { Typography } from "@material-ui/core";
 
-import { withRouter } from "react-router-dom";
+import { withRouter, Route, Switch } from "react-router-dom";
 
 import Header from "./Header";
 import AppMenu from "./AppMenu";
@@ -23,7 +23,26 @@ const Main = ({ user, setUser, setJwt }) => {
           h(
             "div",
             { className: styles.main },
-            h(Typography, { variant: "h6" }, `Welcome back, ${user.username}`)
+            h(
+              Switch,
+              null,
+              h(
+                Route,
+                { exact: true, path: "/app/edit" },
+                h(Typography, { variant: "h2" }, "Edit")
+              ),
+              h(
+                Route,
+                { exact: true, path: "/app/upload" },
+                h(Typography, { variant: "h6" }, "Upload")
+              ),
+
+              h(
+                Route,
+                { exact: true, path: "/app/collection" },
+                h(Typography, { variant: "h6" }, "Collection")
+              )
+            )
           )
         )
       )
