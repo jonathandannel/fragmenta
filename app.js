d8ba7d20b3583fb6b98a4f3a102f4360e71035bf
jonathan dannel <usixjad@gmail.com>
2019-12-22 19:43:37 -0500

    add route for images

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 const appRouter = require("./routes/index");
 const authRouter = require("./routes/api/auth");
 const verifyRouter = require("./routes/api/verify");
+const imageRouter = require("./routes/api/image");
 
 const app = express();
 
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", appRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/verify", verifyRouter);
+app.use("/api/image", imageRouter);
 
 module.exports = app;
