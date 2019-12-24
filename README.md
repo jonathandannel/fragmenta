cf769c442dc8e9ee104bae790a2b23a2a48b5993
jonathan dannel <usixjad@gmail.com>
2019-12-24 15:28:06 -0500

    update readme and sample env

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 - Link it to the app with your credentials via .env
 - Run `npx sequelize db:migrate`
 
+## cloudinary
+
+- Make sure you add keys to `.env`
+
 ## npm start
 
 - Start the express server
@@ -13,3 +17,7 @@
 ## npm run dev (in another terminal)
 
 - Webpack will watch the src/ directory and rebuild the client side bundle when code changes
+
+## npm run build
+
+- Bundle is built the same as `run dev`
--- client/facialDetection.js
+++ client/facialDetection.js
@@ -1,7 +1,7 @@
 import * as faceapi from "face-api.js";
 
 export const loadModels = async loadedCb => {
-  // await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+  await faceapi.nets.faceExpressionNet.loadFromUri("/models");
   await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
   await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
   await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
